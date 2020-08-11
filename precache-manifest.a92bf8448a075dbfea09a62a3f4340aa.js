cfa63e78bb01816fc539924a55ea49599697b162
Chaluemchat Chompuwanichakul <chaluemchat@gmail.com>
2020-08-11 23:39:17 +0700

    Updates

---
+++ precache-manifest.a92bf8448a075dbfea09a62a3f4340aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2d3f434a832b051e3c1a69cc3f99b7",
+    "url": "/crud-react-redux/index.html"
+  },
+  {
+    "revision": "48676a269836a94b2b81",
+    "url": "/crud-react-redux/static/css/main.990bcd04.chunk.css"
+  },
+  {
+    "revision": "2f5749add2f03ab01539",
+    "url": "/crud-react-redux/static/js/2.ac5f4546.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/crud-react-redux/static/js/2.ac5f4546.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48676a269836a94b2b81",
+    "url": "/crud-react-redux/static/js/main.ae412648.chunk.js"
+  },
+  {
+    "revision": "7452d62802bd4f8e9d8f",
+    "url": "/crud-react-redux/static/js/runtime-main.dc22c650.js"
+  }
+]);
